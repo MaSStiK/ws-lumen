05eaa9209edb6c669eecd6b941051df2c3a45f41
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2021-11-04 23:08:04 +0500

    Progress

---
+++ app/Http/Controllers/CardsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use Illuminate\Http\Request;
+
+class CardsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function showAll() {
+        $cards = Card::all();
+        return  response()->json($cards, 200);
+    }
+
+    public function add(Request $request) {
+        $card_text = $request->input('card_text');
+        $card = new Card();
+        $card->card_text = $card_text;
+        $card->save();
+        return  response()->json($card, 201);
+    }
+
+    public function edit(Request $request, $card_id) {
+        $card_text = $request->input('card_text');
+        $card = Card::find($card_id);
+        $card->card_text = $card_text;
+        $card->save();
+        return  response()->json($card, 200);
+    }
+
+    public function delete($card_id) {
+        $card = Card::find($card_id);
+        $card->delete();
+        return  response("Successfully deleted", 200);
+    }
+}
---
+++ app/Models/Card.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    public const CREATED_AT = null;
+    public const UPDATED_AT = null;
+
+    protected $table = 'cards';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['card_text', 'card_likes'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
